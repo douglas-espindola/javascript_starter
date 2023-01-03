2e9f35925e0718cf51cf9671163b52224a7e3b89
Douglas Espindola <douglas.spindola15@gmail.com>
2023-01-02 23:19:04 -0300

    First commit

---
+++ variables/script.js
@@ -0,0 +1,18 @@
+var nome = 'Douglas';
+var idade = '30';
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+
+// Evitar repedir valores
+var preco = 25;
+var totalComprado = 5
+var totalPreco = preco * totalComprado;
+console.log(totalPreco);
+
+// Usando virgula
+var sobrenome = 'Brito', cidade = 'Belém';
+console.log(sobrenome, cidade);
+
+// Sem valor (return undefined)
+var precoAplicativo;
+console.log(precoAplicativo);
