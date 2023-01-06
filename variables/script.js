12918f8a743496de79250c726af66ba7769e0b79
Douglas Espindola <douglas.spindola15@gmail.com>
2023-01-05 21:30:30 -0300

    Finalizado aula sobre variaveis

--- variables/script.js
+++ variables/script.js
@@ -1,18 +1,76 @@
-var nome = 'Douglas';
-var idade = '30';
-var possuiFaculdade = true;
-console.log(nome, idade, possuiFaculdade);
+// var nome = 'Douglas';
+// var idade = '30';
+// var possuiFaculdade = true;
+// console.log(nome, idade, possuiFaculdade);
 
-// Evitar repedir valores
+// Variáveis evitam repetições DRY(Don't repeat youself)
 var preco = 25;
 var totalComprado = 5
 var totalPreco = preco * totalComprado;
-console.log(totalPreco);
+// console.log(totalPreco);
 
-// Usando virgula
+
+// Declarando varias variáveis Usando vírgula
 var sobrenome = 'Brito', cidade = 'Belém';
-console.log(sobrenome, cidade);
+// console.log(sobrenome, cidade);
 
 // Sem valor (return undefined)
 var precoAplicativo;
-console.log(precoAplicativo);
+// console.log(precoAplicativo);
+
+//Hoisting (As variáveis são movidas para cima do código, porém o valor atribuído não é movido).
+// console.log(nome);
+// var outroNome = 'Doug';
+
+// Forma correta
+
+var profissao = 'Designer';
+// console.log(profissao);
+
+
+
+// ===== // Mudar o valor atribuído
+/*
+É possível mudar os valores atribuídos a variáveis declaradas com var e let. Porém não é porém não é possível modificar valores das declaradas com const
+*/
+
+const possuiMestrado = true;
+// possuiMestrado = false
+// return um erro
+
+
+
+
+
+
+
+
+
+
+// Exercício
+
+/*
+Dica rápida, frases (string) devem ser colocadas entre aspas (simples ou duplas) e números não levam aspas.
+*/
+
+// Declarar uma variável com o seu nome
+let meuNome = 'Douglas Brito Espíndola';
+
+// Declarar uma variável com a sua idade
+let minhaIdade = 30;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var minhaComidaPreferida;
+
+// Atribuir valor a sua comida favorita
+minhaComidaPreferida = 'Sequência de Caramrão';
+
+// Declarar 5 variáveis diferentes sem valores
+var aula = 'Aula 2',
+  curso = 'JavaScript',
+  professor = 'André',
+  site = 'origamid.com',
+  ferramenta = 'VSCode';
+
+console.log(meuNome, minhaIdade, minhaComidaPreferida, aula, curso, professor, site, ferramenta);
